4255ae7955ddd80477494f3be21955fdbbb94d7d
jojo69 <jojo00231.jj@gmail.com>
2020-01-03 15:22:50 +0700

    1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,14 @@
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u, double a, double t)
+{
+  return ((u*t)+(0.5*a*t*t)) ;
+}
 
 int main(){
 
-  //Calling findDistance() using test cases
+ cout << findDistance(0,0.5,1) ;
 
   return 0;
 }
