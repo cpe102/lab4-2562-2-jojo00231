c13ee172ea479c4dce29ee49487b100d7b3afae1
jojo69 <jojo00231.jj@gmail.com>
2020-01-03 15:12:10 +0700

    1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
 
+int main(){
+    double x ;
+	cout << "Enter x = ";
+    cin >> x ;
+    x = (3*x*x*x) + 2*2.71828 + pow(2,(2*x)+1) + sqrt((x*x)+1) ;
+    cout << "Result y = " << x ;
+
+    return 0 ;
+}
