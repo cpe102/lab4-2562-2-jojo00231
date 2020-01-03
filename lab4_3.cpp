108e32d608dc73736e31fdfc0ed651c8a76b41c0
jojo69 <jojo00231.jj@gmail.com>
2020-01-03 16:08:29 +0700

    1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int findDivisor(int x){
+    int i=2;
+    
+    while(x>0){
+		if(x%i ==0){
+			return i;
+		}
+		i++;
+	}
+}
+
+	
+
+int main()
+{
+    cout << findDivisor(10) << endl ;
+	cout << findDivisor(97) << endl ;
+	cout << findDivisor(221) << endl ;
+
+    return 0 ;
+}
 
